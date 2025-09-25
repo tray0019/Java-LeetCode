bc44c3e2aeffa45adb7bff1dcf1b9ef4640c9709
tray0019 <tray0019@algonquinlive.com>
2025-09-25 00:09:43 -0400

    Learn and coded dynamic array. Grow and shrink should be automatic but not my code, need to investigate

---
+++ src/Algorithm/DynamicArray.java
@@ -0,0 +1,148 @@
+package Algorithm;
+
+import java.util.ArrayList;
+
+public class DynamicArray {
+	
+	int size;
+	int capacity = 10;
+	Object[] array;
+	
+	public DynamicArray() {
+		this.array = new Object[capacity];
+	}
+	
+	public DynamicArray(int capacity) {
+		this.capacity = capacity;
+		this.array = new Object[capacity];
+	}
+	
+	public void add(Object data) {
+		if(size > capacity) {
+			grow();
+		}
+		array[size] = data;
+		size++;
+	}
+	
+	public void insert(int index, Object data) {
+		if(size > capacity) {
+			grow();
+		}
+		
+		for(int i = size; i < index; i++) {
+			array[i] = array[index-1];
+		}
+		
+		array[index] = data;
+		size++;
+	}
+	
+	public void delete(Object data) {
+		for(int i = 0; i< size; i++) {
+			if(array[i] == data) {
+				for(int j = 0; j <(size - i -1);j++) {
+					array[i + j]= array[i+j+1];
+				}
+				array[size-1] = null;
+				size--;
+				if(size <= (int) capacity/3) {
+					shrink();
+				}
+				break;
+			}
+		}
+	}
+	
+	public int search(Object data) {
+		
+		for(int i = 0; i<size; i++) {
+			if(array[i] == data) {
+				return i;
+			}
+		}
+		return -1;
+		
+	}
+	
+	private void grow() {
+		
+		int newCapacity = (int)(capacity*2);
+		Object[] newArray = new Object[newCapacity];
+		
+		for(int i =0; i < size; i++) {
+			newArray[i] = array[i];
+			
+		}
+		
+		capacity = newCapacity;
+		array = newArray;
+	}
+
+	private void shrink() {
+		int newCapacity = (int)(capacity/2);
+		Object[] newArray = new Object[newCapacity];
+		
+		for(int i =0; i < size; i++) {
+			newArray[i] = array[i];
+			
+		}
+		
+		capacity = newCapacity;
+		array = newArray;
+	}
+	
+	public boolean empty() {
+		return size == 0;
+		
+	}
+	
+	public String toString() {
+		
+		String string = "";
+		for(int i = 0; i < capacity; i++) {
+			string += array[i] +" ,";
+		}
+		
+		if(string != "") {
+			string = "["+string.substring(0, string.length()-2)+ "]";
+		}else {
+			string = "[]";
+		}
+		
+		return string;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		//ArrayList<String> arrayList = new ArrayList<>();
+		
+		DynamicArray dynamicArray = new DynamicArray(5);
+		
+		System.out.println(dynamicArray.capacity);
+		
+		//dynamicArray.grow();
+		dynamicArray.add("A");
+		dynamicArray.add("B");
+		dynamicArray.add("C");
+		dynamicArray.add("D");
+		dynamicArray.add("E");
+		dynamicArray.add("F");
+		//dynamicArray.insert(9,"Z");
+		
+		System.out.println(dynamicArray.search("C"));
+		
+		System.out.println(dynamicArray);
+		System.out.println("Size: "+dynamicArray.size);
+		System.out.println("Dynamic: "+dynamicArray.capacity);
+		System.out.println("Empty: "+dynamicArray.empty());
+		
+		dynamicArray.delete("A");
+		System.out.println(dynamicArray);
+		
+		
+
+	}
+
+}
