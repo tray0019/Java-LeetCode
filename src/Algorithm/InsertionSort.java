595fe24462ba20c4db92ab5e806ac1e58aabc883
tray0019 <tray0019@algonquinlive.com>
2025-10-07 13:11:25 -0400

    added insertion sort, high run time complexity

---
+++ src/Algorithm/InsertionSort.java
@@ -0,0 +1,47 @@
+package Algorithm;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	/* insertion sort - afer comparing elements to the left
+	 *   				shift elements to the right to make room to insert a value
+	 * 
+	 * 
+	 * 				Quadratic time O(n^2)
+	 * 				small data set = decent
+	 * 				large data set = BAD
+	 * 
+	 * 				Less steps than bubble Sort
+	 * 		 		Best case is O(n) compared to Selection Sort O(n^2)
+	 */
+	public static void main(String[] args) {
+		
+		int array[] = {9,1,8,2,7,3,7,4,6,5};
+		
+		System.out.println(Arrays.toString(array));
+		
+		insertionSort(array);
+
+		System.out.println(Arrays.toString(array));
+	}
+
+	private static void insertionSort(int[] array) {
+		
+		for(int i = 1;i < array.length; i++) {
+			int temp = array[i];
+			int j = i-1;
+			
+			while(j >= 0 && array[j] > temp) {
+				array[j+1] = array[j];
+				j--;
+			}
+
+			array[j + 1] = temp;
+		}
+		
+	}
+	
+	
+
+}
