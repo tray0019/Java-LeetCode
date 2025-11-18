b28e31b10ca20745e8a167fad32362b9fe5550ec
tray0019 <tray0019@algonquinlive.com>
2025-11-18 15:03:30 -0500

    Practicing longest substring

--- src/CoreConcepts/LongestSubString.java
+++ src/CoreConcepts/LongestSubString.java
@@ -9,38 +9,58 @@ public class LongestSubString {
 		
 		   String s = "abczzabcbadefg";
 	       int result = lengthOfLongestSubstring(s);
-	       System.out.println("Length of longest substring without repeating characters: " + result);
+	       System.out.println("Length of longest substring without \nrepeating characters: " + result);
 	    
 	}
 
 	private static int lengthOfLongestSubstring(String s) {
-		HashSet<Character> windows = new HashSet<>();
+		//abczzabcbadefg
 		
+		System.out.println("Step 1: "+s.length());
+		HashSet<Character> windows = new HashSet<>();
 		int left = 0;
 		int maxLength = 0;
 		
 		for(int right = 0; right<s.length();right++) {
 			char c = s.charAt(right);
-			System.out.println(right+": "+c);
+
+			System.out.println("Step "+right+": " +c);
 			
 			while(windows.contains(c)) {
-				//System.out.println(right+": "+windows.contains(c));
-				//System.out.println("remove: "+s.charAt(left));
-				System.out.println("remove index "+left+": "+s.charAt(left));
-				
+				System.out.println(right+": "+c+" | "+windows.contains(c));
 				windows.remove(s.charAt(left));
+				
 				left++;
-								
 			}
 			
 			windows.add(c);
 			maxLength = Math.max(maxLength, right-left+1);
-			System.out.println(right+": Max Length "+maxLength+" -left:"+left);
 			
 		}
-		
+		System.out.println(windows.toString());
 		return maxLength;
 		
+		
+		
+		
+		
+		/*
+		HashSet<Character> windows = new HashSet<>();
+		
+		int left = 0;
+		int maxLength = 0;
+		for(int right = 0; right<s.length();right++) {
+			char c = s.charAt(right);
+			while(windows.contains(c)) {
+				windows.remove(s.charAt(left));
+				left++;
+								
+			}
+			windows.add(c);
+			maxLength = Math.max(maxLength, right-left+1);
+		}//System.out.println(windows.toString());
+		return maxLength;
+		*/
 	}
 
 }
