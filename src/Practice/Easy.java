b85676428e24771df42add5df52bc7fbde12313a
tray0019 <tray0019@algonquinlive.com>
2026-08-16 18:04:37 -0400

    practice - lett code not bad

--- src/Practice/Easy.java
+++ src/Practice/Easy.java
@@ -33,8 +33,9 @@ System.out.println("findFirstIndex: "+findFirstIndex(nums2,target));
 System.out.println("reverseString: "+reverseString("CAt"));
 System.out.println("reverseString2: "+reverseString2("hello"));
 System.out.println("isPalindrome: "+isPalindrome("hoh"));
+System.out.println("twoSum: "+twoSum(nums,7));
 
-		
+
 
 		
 
@@ -167,6 +168,24 @@ System.out.println("isPalindrome: "+isPalindrome("hoh"));
 		}
 		
 		return true;
+
+	}
+	
+	public static int[] twoSum(int[] nums, int target) {
+		
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = i+1; j<nums.length; j++) {
+				if(nums[i]+nums[j] == target) {
+					System.out.println(nums[i]+" + "+nums[j]+" = "+target);
+					return new int[] {i,j};
+					
+				}
+			}
+		}
+		
+		return new int[] {};
+		
+		
 
 	}
 
